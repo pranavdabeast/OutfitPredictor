ca4bed4e8569f8a404b89467e477dcc3af5c8636
pranavdabeast <54599543+pranavdabeast@users.noreply.github.com>
2020-04-08 15:57:20 -0500

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 var forecastData = [Weather]()
 
+//func shorten12(array: [Weather]) -> Array<Double> {
+//
+//    var temperaturearray = [Double](arrayLiteral: 12)
+//    var itemcount = 0
+//
+//    for items in array {
+//
+//        if itemcount < 11 {
+//            temperaturearray[itemcount] = items
+//            itemcount = itemcount + 1
+//        }
+//
+//    }
+//
+//    return(temperaturearray)
+//}
+
+var arraytemp = [Double]()
 
 
 class ViewController: UIViewController {
@@ -17,15 +35,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
          Weather.forecast(withLocation: "32.7767,-96.7970") { (results:[Weather]) in
-//        for result in results{
-//
-//                print("\(result)\n\n")
-//                    }
+        var itemcounter = 0
+        for result in results{
+
+                print("\(result)\n\n")
+            arraytemp[itemcounter] = results[itemcounter].apparentTemperature
+                itemcounter = itemcounter + 1
+                print(arraytemp[itemcounter])
+            }
             
             print(results[0].apparentTemperature)
-            
+//            print(shorten12(results: results.apparentTemperature))
             
         // Do any additional setup after loading the view.
+            
+            
             }
         
         }
