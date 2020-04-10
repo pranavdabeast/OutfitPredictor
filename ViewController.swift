1bfe6d53fb04479732111913f09c4c9a41866306
pranavdabeast <54599543+pranavdabeast@users.noreply.github.com>
2020-04-10 18:57:09 -0500

    Update ViewController.swift
    Changed the shorten12 function to avoid fatal errors in threads

--- ViewController.swift
+++ ViewController.swift
@@ -9,46 +9,51 @@
 import UIKit
 var forecastData = [Weather]()
 
-//func shorten12(array: [Weather]) -> Array<Double> {
-//
-//    var temperaturearray = [Double](arrayLiteral: 12)
-//    var itemcount = 0
-//
-//    for items in array {
-//
-//        if itemcount < 11 {
-//            temperaturearray[itemcount] = items
-//            itemcount = itemcount + 1
-//        }
-//
-//    }
-//
-//    return(temperaturearray)
-//}
-
-var arraytemp = [Double]()
-
-
 class ViewController: UIViewController {
+    
+    
+
+    func shorten12(array: [Weather]) -> Array<Double> {
+
+        var temperaturearray = [Double]()
+        var itemcount = 0
+            
+        for items in array {
+
+            if itemcount < 11 {
+                temperaturearray.append(items.apparentTemperature)
+                itemcount = itemcount + 1
+            }
+
+        }
+
+        return(temperaturearray)
+    }
+
+   // var arraytemp = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var temperaturearray = [Double](arrayLiteral: 12)
+        
          Weather.forecast(withLocation: "32.7767,-96.7970") { (results:[Weather]) in
-        var itemcounter = 0
+            
+          temperaturearray = self.shorten12(array:results)
+
+            for tempitems in temperaturearray {
+                print(tempitems)
+            }
+
         for result in results{
 
-                print("\(result)\n\n")
-            arraytemp[itemcounter] = results[itemcounter].apparentTemperature
-                itemcounter = itemcounter + 1
-                print(arraytemp[itemcounter])
+        print(result)
+
             }
-            
-            print(results[0].apparentTemperature)
-//            print(shorten12(results: results.apparentTemperature))
+//            print(results[0].apparentTemperature)
+//
             
         // Do any additional setup after loading the view.
-            
             
             }
         
