602189716ab3981f9ec648eb211e897aba339303
pranavdabeast <54599543+pranavdabeast@users.noreply.github.com>
2020-04-13 19:16:37 -0500

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 //
 //  ViewController.swift
 //  Outfit Recommender
@@ -7,21 +9,20 @@
 //
 
 import UIKit
-var forecastData = [Weather]()
+var forecastData = [Weather]() //Array to impoort weather objects into
 
 class ViewController: UIViewController {
     
-    
-
-    func shorten12(array: [Weather]) -> Array<Double> {
-
+//Function that shortens list of 48 hourly data objects to 12- takes in array of weather objects and outputs array of doubles
+    func shorten12temp(array: [Weather]) -> Array<Double>  {
+        
         var temperaturearray = [Double]()
         var itemcount = 0
             
         for items in array {
 
-            if itemcount < 11 {
-                temperaturearray.append(items.apparentTemperature)
+            if itemcount < 12 {
+                temperaturearray.append(items.apparentTemperature) //appends temperature(double) into a new array
                 itemcount = itemcount + 1
             }
 
@@ -29,38 +30,206 @@ class ViewController: UIViewController {
 
         return(temperaturearray)
     }
+    
+    func shorten12time(array: [Weather]) -> Array<Double> {
+        
+        var timearray = [Double]()
+        var itemcount = 0
+        
+        for items in array {
+
+            if itemcount < 12 {
+                timearray.append(items.time)//appends temperature(double) into a new array
+                itemcount = itemcount + 1
+            }
+
+        }
+
+        return(timearray)
+        
+    }
+
+//Function that predicts clothes for 1)average temperature and 2) any temperature spikes or dips. Takes in array of 12 double values and ouputs array of strings
+    func Predictclothes(temp12: Array<Double>, times : Array<Double> /*timehour: Int*/) -> Array<String> {
+        
+        let seventyabove = "shorts and T-shirt" //Hypothetical preferences
+        let fortyabove = "jeans/pants and jacket"
+        let fortybelow = "heavy coat"
+        let dict = [1 : "shorts and T-shirt", 2 : "jeans/pants and jacket", 3 : "heavy coat"] // For potential use of dictionaries
+        
+        var totalsum = 0 //used for average calculation
+        var itemcount = 0 //used ot count the total number of items in order to calculate average
+        var thresholdmin : Double = 0 //used to determine levels for temperature classification
+        var thresholdmax : Double = 0
+        var index = 0 //simple index for use in for loops
+        var tempspikeresults = [String]() //array for addition of strings
+        var clothespredictorarray = [String]()
+        var dictindex = 0 //for potential use of dictionaries
+        
+        //Averaging func
+        for hours in temp12 {
+            totalsum = totalsum + Int(hours)
+            itemcount = itemcount + 1
+        }
+        let average = (totalsum / itemcount)
+        print("Average Temperature over the next 12 hours = \(average)")
+        
+        
+        /* The following chunk of code is a general predictor for clothes based on the average temperature over the next 12 hours. It also establishes the ranges for certain types of clothing*/
+        if average > 70 {
+            clothespredictorarray.append(seventyabove) //Adds general clothing reccomendation to final clothing array
+            thresholdmin = 70
+            thresholdmax = 120
+            dictindex = 1 //for dictionary
+        }
+        if average < 70 && average > 40 {
+            clothespredictorarray.append(fortyabove)
+            thresholdmin = 40
+            thresholdmax = 70
+            dictindex = 2 //for dictionary
+            
+        }
+        if average < 40 {
+            clothespredictorarray.append(fortybelow)
+            thresholdmin = 0
+            thresholdmax = 40
+            dictindex = 3 //for dictionary
+        }
+//        print("threshold min == \(thresholdmin)")
+//        print("threshold max == \(thresholdmax)")
+        
+        
+        
+        let highnumber = thresholdmax + 30 //To establish boundaries for the other categories of temperature/clothing
+        let lownumber = thresholdmin - 30
+        
+        //Checks if temperature jumps into a higher or lower category and appends the result to temperaturespikearray
+        for temp in temp12 {
+            index = index + 1
+            if temp > thresholdmin && temp < thresholdmax {
+                tempspikeresults.append("Temperature returns to average")
+            
+            }
+            
+            if temp > thresholdmax && temp < highnumber {
+                tempspikeresults.append("Temperature raises to higher domain")
+                dictindex = dictindex - 1
+                
+            }
+                
+            if temp < thresholdmin && temp > lownumber {
+                tempspikeresults.append("Temperature dips to lower domain")
+                dictindex = dictindex + 1
+            }
+            
+        }
+        
+        index = 0 //Re-using index
+        
+
+        //Appends time for each temperature spike or drop and reccomends clothing for those spikes or drops
+        for items in tempspikeresults {
+            
+            if items == "Temperature returns to average" {
+//                var militaryhour = timehour + index
+//
+//                if militaryhour > 24 {
+//                    militaryhour = militaryhour - 24
+//                }
+            }
+            
+            if items == "Temperature raises to higher domain" {
+                
+                
+//                var militaryhour = timehour + index //Adds index number to current time to find when spike will occur
+//                 if militaryhour > 24 {
+//                     militaryhour = militaryhour - 24 //To loop hours back into 24-hour format
+//                 }
+                clothespredictorarray.append("\(items) at \(times[index])") //Appends weather change notification + time to final array
+
+                /* if dictindex == 1 {
+                    print("Wear \(seventyabove)")
+                }
+
+                if dictindex == 2 {
+                    print("Wear \(fortyabove)")
+                } */
+                
+            }
+            
+            if items == "Temperature dips to lower domain" {
+                
+//                var militaryhour = timehour + index
+//                 if militaryhour > 24 {
+//                     militaryhour = militaryhour - 24
+//                 }
+                
+                 clothespredictorarray.append("\(items) at \(times[index])") //Appends weather change notification + time to final array
+                
+                /* if dictindex == 2 {
+                    print("Wear \(fortyabove)")
+                }
+                
+                if dictindex == 3 {
+                    print("Wear \(fortybelow)")
+                } */
+                
+            }
+            
+            index = index + 1
+        }
+        return(clothespredictorarray)
+    }
+    
 
    // var arraytemp = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var temperaturearray = [Double](arrayLiteral: 12)
+        var temperaturearray = [Double]()
+        var timearray = [Double]()
+        var clothespredictorarray = [String]()
+        
+//        // *** Create date ***
+//        let date = Date()
+//
+//        // *** create calendar object ***
+//        var calendar = Calendar.current
+//
+//        // *** Get components using current Local & Timezone ***
+//        calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+//
+//        // *** define calendar components to use as well Timezone to UTC ***
+//        calendar.timeZone = TimeZone(identifier: "UTC")!
+//
+//        // *** Get All components from date ***
+//        let components = calendar.dateComponents([.hour, .year, .minute], from: date)
+////        print("All Components : \(components)")
+//
+//        // *** Get Individual components from date ***
+//        let UTChour = calendar.component(.hour, from: date)
+//        let hour = UTChour - 5
         
          Weather.forecast(withLocation: "32.7767,-96.7970") { (results:[Weather]) in
             
-          temperaturearray = self.shorten12(array:results)
-
-            for tempitems in temperaturearray {
-                print(tempitems)
-            }
-
-        for result in results{
-
-        print(result)
-
-            }
-//            print(results[0].apparentTemperature)
-//
+            temperaturearray = self.shorten12temp(array:results) //Caling shorten 12 by passing an array, called "array" of results
             
+            timearray = self.shorten12time(array: results) //Calling shorten12 for the times
+            
+            clothespredictorarray = self.Predictclothes(temp12: temperaturearray, times: timearray/*,timehour: hour*/)
+            
+            for items in clothespredictorarray {
+                print(items)
+            }
+        
+//            print(results[0].apparentTemperature)
+
         // Do any additional setup after loading the view.
             
             }
-        
         }
-    
-    
-
 }
+
 
 
