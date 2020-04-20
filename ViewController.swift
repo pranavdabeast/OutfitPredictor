77d5839f4942c9dae1df63d757844bb8336ab282
pranavdabeast <54599543+pranavdabeast@users.noreply.github.com>
2020-04-19 22:34:48 -0500

    Update ViewController.swift
    Finished connecting UI to algorithms. 
    App is fully functional

--- ViewController.swift
+++ ViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 import Foundation
-var forecastData = [Weather]() //Array to impoort weather objects into
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
-//Function that shortens list of 48 hourly data objects to 12- takes in array of weather objects and outputs array of doubles
-    func shorten12temp(array: [Weather]) -> Array<Double>  {
+    let defaults = UserDefaults.standard //Userdefaults to store clothing preferences
+    @IBOutlet weak var seventyabovefield: UITextField! //Outlets to connect buttons and text fields
+    @IBOutlet weak var fortyabovefield: UITextField!
+    @IBOutlet weak var fortybelowfield: UITextField!
+    @IBOutlet weak var textdisplay: UITextView!
+    
+    struct Keys {
+        static let seventyaboveweather = "seventyaboveweather"
+        static let fortyaboveweather = "fortyaboveweather"
+        static let fortybelowweather = "fortybelowweather"
+    } //To prevent spelling errors in keys for Defaults
+    
+
+    func shorten12Temp(array: [Weather]) -> Array<Double>  {
         
         var temperaturearray = [Double]()
         var itemcount = 0
@@ -21,16 +32,16 @@ class ViewController: UIViewController {
         for items in array {
 
             if itemcount < 12 {
-                temperaturearray.append(items.apparentTemperature) //appends temperature(double) into a new array
+                temperaturearray.append(items.apparentTemperature) //Appends temperature(double) into a new array
                 itemcount = itemcount + 1
             }
 
         }
 
         return(temperaturearray)
-    }
+    } //Function that shortens list of 48 hourly data objects to 12- takes in array of weather objects and outputs array of doubles
     
-    func shorten12time(array: [Weather]) -> Array<Double> {
+    func shorten12Time(array: [Weather]) -> Array<Double> {
         
         var timearray = [Double]()
         var itemcount = 0
@@ -38,17 +49,17 @@ class ViewController: UIViewController {
         for items in array {
 
             if itemcount < 12 {
-                timearray.append(items.time)//appends temperature(double) into a new array
+                timearray.append(items.time)//Appends times(double) into a new array
                 itemcount = itemcount + 1
             }
 
-        }
+        } //Function that shortens list of 48 hourly data objects to 12- takes in array of weather objects and outputs array of doubles
 
         return(timearray)
         
     }
 
-    func UNIXconverter(timearray : Array<Double>) -> Array<String> {
+    func unixConverter(timearray : Array<Double>) -> Array<String> {
         var timestrings = [String]()
         
         for times in timearray {
@@ -62,59 +73,53 @@ class ViewController: UIViewController {
                            
                    }
         return timestrings
-    }
-//Function that predicts clothes for 1)average temperature and 2) any temperature spikes or dips. Takes in array of 12 double values and ouputs array of strings
-    func Predictclothes(temp12: Array<Double>, times : Array<String> /*timehour: Int*/) -> Array<String> {
+    }// Function that converts UNIX-style times into hour-minute formats
+    
+    func predictClothes(temp12: Array<Double>, times : Array<String>) -> Array<String> {
         
-        let seventyabove = "shorts and T-shirt" //Hypothetical preferences
-        let fortyabove = "jeans/pants and jacket"
-        let fortybelow = "heavy coat"
-//        let dict = [1 : "shorts and T-shirt", 2 : "jeans/pants and jacket", 3 : "heavy coat"] // For potential use of dictionaries
+        let seventyabove = defaults.value(forKey: Keys.seventyaboveweather) as? String ?? "" //Preferences from User Defaults
+        let fortyabove = defaults.value(forKey: Keys.fortyaboveweather) as? String ?? ""
+        let fortybelow = defaults.value(forKey: Keys.fortybelowweather) as? String ?? ""
         
-        var totalsum = 0 //used for average calculation
-        var itemcount = 0 //used ot count the total number of items in order to calculate average
-        var thresholdmin : Double = 0 //used to determine levels for temperature classification
+        var totalsum = 0 //Used for average calculation
+        var itemcount = 0 //Used to count the total number of items in order to calculate average
+        var thresholdmin : Double = 0 //Used to determine levels for temperature classification
         var thresholdmax : Double = 0
-        var index = 0 //simple index for use in for loops
-        var tempspikeresults = [String]() //array for addition of strings
+        var index = 0 //Simple index for use in for loops
+        var tempspikeresults = [String]() //Array for addition of strings
         var clothespredictorarray = [String]()
-        var dictindex = 0 //for potential use of dictionaries
-        var prevstring:String = "" //for comparisons in weather spikes
+        var dictindex = 0 //For domain jumps in spikes/dips
+        var prevstring:String = "" //For comparisons to previous weather spikes
         
-        //Averaging func
+        
         for hours in temp12 {
             totalsum = totalsum + Int(hours)
             itemcount = itemcount + 1
-        }
-        let average = (totalsum / itemcount)
-//        print("Average Temperature over the next 12 hours = \(average)")
+        }//Averaging function
         
+        let average = (totalsum / itemcount)
         
         /* The following chunk of code is a general predictor for clothes based on the average temperature over the next 12 hours. It also establishes the ranges for certain types of clothing*/
         if average > 70 {
             clothespredictorarray.append("Average Temperature over the next 12 hours = \(average). Wear \(seventyabove)") //Adds general clothing reccomendation to final clothing array
             thresholdmin = 70
             thresholdmax = 120
-            dictindex = 1 //for dictionary
+            dictindex = 1
         }
         if average < 70 && average > 40 {
             clothespredictorarray.append("Average Temperature over the next 12 hours = \(average). Wear \(fortyabove)")
             thresholdmin = 40
             thresholdmax = 70
-            dictindex = 2 //for dictionary
+            dictindex = 2
             
         }
         if average < 40 {
             clothespredictorarray.append("Average Temperature over the next 12 hours = \(average). Wear \(fortybelow)")
             thresholdmin = 0
             thresholdmax = 40
-            dictindex = 3 //for dictionary
+            dictindex = 3
         }
-//        print("threshold min == \(thresholdmin)")
-//        print("threshold max == \(thresholdmax)")
-        
-        
-        
+
         let highnumber = thresholdmax + 30 //To establish boundaries for the other categories of temperature/clothing
         let lownumber = thresholdmin - 30
         
@@ -144,19 +149,19 @@ class ViewController: UIViewController {
         //Appends time for each temperature spike or drop and reccomends clothing for those spikes or drops
         for items in tempspikeresults {
             
-            if index == 0 {
-                prevstring = items
-            }
-            
             if items == "Temperature returns to average" {
+                
+                if index == 0 {
+                    prevstring = items
+                }
                 
             }
             
             if items == "Temperature raises" {
-                if items != prevstring {
+                
+                if index == 0 || items != prevstring {
                     dictindex = dictindex - 1
                 }
-//                clothespredictorarray.append("\(items) at \(times[index])") //Appends weather change notification + time to final array
 
                 if dictindex == 1 {
                     clothespredictorarray.append("\(items) at \(times[index]). Wear \(seventyabove)")
@@ -170,10 +175,9 @@ class ViewController: UIViewController {
             
             if items == "Temperature dips" {
                 
-                if items != prevstring {
-                    dictindex = dictindex + 1
+                if index == 0 || items != prevstring {
+                    dictindex = dictindex - 1
                 }
-//                clothespredictorarray.append("\(items) at \(times[index]).") //Appends weather change notification + time to final array
                 
                 if dictindex == 2 {
                     clothespredictorarray.append("\(items) at \(times[index]). Wear \(fortyabove)")
@@ -191,57 +195,73 @@ class ViewController: UIViewController {
         return(clothespredictorarray)
     }
     
-
-   // var arraytemp = [Double]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        seventyabovefield.delegate = self
+        fortyabovefield.delegate = self
+        fortybelowfield.delegate = self
+        
+        checkForSavedPreferences()
+
+    }//Sets delegates and fills in saved preferences
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        saveWeatherPreferences()
+    } //Calls WeatherPReferences
+    
+    func saveWeatherPreferences() {
+        defaults.set(seventyabovefield.text!, forKey: Keys.seventyaboveweather)
+        defaults.set(fortyabovefield.text!, forKey: Keys.fortyaboveweather)
+        defaults.set(fortybelowfield.text!, forKey: Keys.fortybelowweather)
+
+    } //Sets input information as default
+    
+    func checkForSavedPreferences() {
+        let seventyaboveclothingpreference = defaults.value(forKey: Keys.seventyaboveweather) as? String ?? ""
+        let fortyaboveclothingpreference = defaults.value(forKey: Keys.fortyaboveweather) as? String ?? ""
+        let fortybelowclothingpreference = defaults.value(forKey: Keys.fortybelowweather) as? String ?? ""
+        seventyabovefield.text = seventyaboveclothingpreference
+        fortyabovefield.text = fortyaboveclothingpreference
+        fortybelowfield.text = fortybelowclothingpreference
+    } //Fills in default information when app is opened
+    
+    @IBAction func predictButtonTapped(_ sender: UIButton) {
+        
+        var forecastData = [Weather]() //Array to import weather objects into
         var temperaturearray = [Double]()
         var timearray = [Double]()
         var clothespredictorarray = [String]()
         var timestrings = [String]()
-        
-//        // *** Create date ***
-//        let date = Date()
-//
-//        // *** create calendar object ***
-//        var calendar = Calendar.current
-//
-//        // *** Get components using current Local & Timezone ***
-//        calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
-//
-//        // *** define calendar components to use as well Timezone to UTC ***
-//        calendar.timeZone = TimeZone(identifier: "UTC")!
-//
-//        // *** Get All components from date ***
-//        let components = calendar.dateComponents([.hour, .year, .minute], from: date)
-////        print("All Components : \(components)")
-//
-//        // *** Get Individual components from date ***
-//        let UTChour = calendar.component(.hour, from: date)
-//        let hour = UTChour - 5
-        
-         Weather.forecast(withLocation: "32.7767,-96.7970") { (results:[Weather]) in
-            
-            temperaturearray = self.shorten12temp(array:results) //Caling shorten 12 by passing an array, called "array" of results
-            
-            timearray = self.shorten12time(array: results) //Calling shorten12 for the times
-            
-            timestrings = self.UNIXconverter(timearray: timearray)
-            
-            clothespredictorarray = self.Predictclothes(temp12: temperaturearray, times: timestrings/*,timehour: hour*/)
-            
-            for items in clothespredictorarray {
-                print(items)
-            }
-        
-//            print(results[0].apparentTemperature)
 
-        // Do any additional setup after loading the view.
-            }
+        Weather.forecast(withLocation: "32.7767,-96.7970") { (results:[Weather]) in
+
+                temperaturearray = self.shorten12Temp(array:results) //Caling shorten 12 by passing an array, called "array" of results
+
+                timearray = self.shorten12Time(array: results) //Calling shorten12 for the times
+
+                timestrings = self.unixConverter(timearray: timearray)//Calling unixConverter
+
+                clothespredictorarray = self.predictClothes(temp12: temperaturearray, times: timestrings)
+                            
+                let joinedclothespredictorarray = clothespredictorarray.joined(separator: "\n")
+                            
+                DispatchQueue.main.async {
+                    self.textdisplay.text = "\(joinedclothespredictorarray)"
+                }//Displays clothespredictorarray on phone screen
+
         }
+        
+    }//When predictButton is tapped, the main code is run with the algorithm.
+
 }
-
-
-
+    
+    
+    
+//extension ViewController: UITextFieldDelegate {
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//}
